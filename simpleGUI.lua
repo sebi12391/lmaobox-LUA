a1426dcb0565adbe9e423e9475ae5eca47126b43
cBass <sebi.krawiec@gmail.com>
2022-04-17 04:11:02 -0500

    Update #1
    Added a kill logger and cleaned up the code a bit

--- simpleGUI.lua
+++ simpleGUI.lua
@@ -6,10 +6,19 @@ local yVM = 0
 local zVM = -10
 
 
---local tahoma = draw.CreateFont("Tahoma Bold", 15, 590, 0x200)
-local arial = draw.CreateFont("Arial Bold", 15, 590, 0x200)
+local totalKills = 0
 local current_fps = 0
 local ping = 0
+local arial = draw.CreateFont("Arial Bold", 15, 590, 0x200)
+
+--function that determines if you've killed someone
+local function event_hook(ev)
+  if ev:GetName() ~= "player_death" then return end
+  local attacker = entities.GetByUserID(ev:GetInt("attacker"))
+  local localplayer = entities.GetLocalPlayer()
+  if attacker ~= localplayer then return end
+  totalKills = totalKills + 1
+end
 
 
 function RGBRainbow(frequency)
@@ -27,12 +36,13 @@ local function primary()
     current_fps = math.floor(1 / globals.FrameTime())
     if clientstate.GetClientSignonState() == 0 then
       ping = "Not In a Server"
+      totalKills = "Not In a Server"
     elseif clientstate.GetClientSignonState() > 1 and clientstate.GetClientSignonState() < 6 then
       ping = "Connecting To Server"
+      totalKills = 0
     else
       ping = math.floor((clientstate.GetLatencyOut() - clientstate.GetLatencyIn()) * 1000)
-      --ping = math.floor(10/clientstate.GetLatencyOut())
-      --ping = math.floor(10/clientstate.GetDeltaTick())
+
     end
   end
   if overrideViewmodels == 1 then
@@ -44,17 +54,18 @@ local function primary()
 
   draw.SetFont(arial)
   draw.Color(r, g, b, 255)
-  draw.Text(15, 320, "hi there!")
+  draw.Text(15, 320, "hi cBass :)")
   draw.Text(15, 340, "FrameRate:  " .. current_fps .. "")
   draw.Text(15, 360, "Real Ping:  " .. ping .. "")
-  if overrideViewmodels == 1 then
+  draw.Text(15, 380, "Total Kills: " .. totalKills .. "")
+  if overriwadeViewmodels == 1 then
     draw.Color(r, g, b, 255)
-    draw.Text(15, 520, "x ViewModel:  " .. xVM .. "")
-    draw.Text(15, 540, "y ViewModel:  " .. yVM .. "")
-    draw.Text(15, 560, "z ViewModel:  " .. zVM .. "")
+    draw.Text(15, 540, "x ViewModel:  " .. xVM .. "")
+    draw.Text(15, 560, "y ViewModel:  " .. yVM .. "")
+    draw.Text(15, 580, "z ViewModel:  " .. zVM .. "")
   else
     draw.Color(255, 255, 255, 255)
-    draw.Text(15, 520, "Override Viewmodels Not Enabled")
+    draw.Text(15, 540, "Override Viewmodels Not Enabled")
   end
 end
 
@@ -88,67 +99,68 @@ local function secondary()
   -- checks if the aimbot is enabled then renders text
   if (aimbot == 1) then
     draw.Color(r, g, b, 255)
-    draw.Text(15, 380, "AimBot: " .. aimbotfov .. "/" .. string.upper(aimbotMethod) .. "")
+    draw.Text(15, 400, "AimBot: " .. aimbotfov .. " / " .. string.upper(aimbotMethod) .. "")
   else
     draw.Color(255, 255, 255, 255)
-    draw.Text(15, 380, "AimBot: OFF")
+    draw.Text(15, 400, "AimBot: OFF")
   end
 
   -- checks if ESP is enabled then renders text
   if (esp == 1) then
     draw.Color(r, g, b, 255)
-    draw.Text(15, 400, "ESP: ON")
+    draw.Text(15, 420, "ESP: ON")
   else
     draw.Color(255, 255, 255, 255)
-    draw.Text(15, 400, "ESP: OFF")
+    draw.Text(15, 420, "ESP: OFF")
   end
 
   --checks if AA is enabled and renders text
   if (aa == 1) then
     draw.Color(r, g, b, 255)
-    draw.Text(15, 420, "AA:ON")
+    draw.Text(15, 440, "AA:ON")
   else
     draw.Color(255, 255, 255, 255)
-    draw.Text(15, 420, "AA: OFF")
+    draw.Text(15, 440, "AA: OFF")
   end
 
   -- checks if BunnyHop is enabled then renders text
   if (bhop == 1) then
     draw.Color(r, g, b, 255)
-    draw.Text(15, 440, "Bunnyhop: ON")
+    draw.Text(15, 460, "Bunnyhop: ON")
   else
     draw.Color(255, 255, 255, 255)
-    draw.Text(15, 440, "Bunnyhop: OFF")
+    draw.Text(15, 460, "Bunnyhop: OFF")
   end
 
   -- checks if FOV chamger is on and displays it
   if (customfov == 1) then
     draw.Color(r, g, b, 255)
-    draw.Text(15, 460, "Custom FOV: " .. fovvalue .. "")
+    draw.Text(15, 480, "Custom FOV: " .. fovvalue .. "")
   else
     draw.Color(255, 255, 255, 255)
-    draw.Text(15, 460, "Custom FOV: OFF")
+    draw.Text(15, 480, "Custom FOV: OFF")
   end
 
   --checks to see if fake ping is on and displays it
   if (fakeping == 1) then
     draw.Color(r, g, b, 255)
-    draw.Text(15, 480, "Fake Ping: " .. fakepingvalue .. "")
+    draw.Text(15, 500, "Fake Ping: " .. fakepingvalue .. "")
   else
     draw.Color(255, 255, 255, 255)
-    draw.Text(15, 480, "Fake Ping: OFF")
+    draw.Text(15, 500, "Fake Ping: OFF")
   end
 
     --checks to see if fake lag is on and displays it
     if (fakeLag == 1) then
       draw.Color(r, g, b, 255)
-      draw.Text(15, 500, "Fake Lag: " .. fakeLagAmount .. "")
+      draw.Text(15, 520, "Fake Lag: " .. fakeLagAmount .. "")
     else
       draw.Color(255, 255, 255, 255)
-      draw.Text(15, 500, "Fake Lag: OFF")
+      draw.Text(15, 520, "Fake Lag: OFF")
     end
 
 end
 
+callbacks.Register("FireGameEvent", "unique_event_hook", event_hook)
 callbacks.Register("Draw", primary)
 callbacks.Register("Draw", "draw", secondary)
